0ac081ab1d45754960883cb79ce4dd3d34f2161e
Yernur Melsov <ernurmelsov@gmail.com>
2025-01-21 15:53:53 +0500

    fix again

--- main.py
+++ main.py
@@ -244,4 +244,4 @@ async def on_shutdown():
     scheduler.shutdown()
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+    uvicorn.run(app, host="0.0.0.0", port=8000)
